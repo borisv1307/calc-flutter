5dc7e03a49633bb69bb07c2d1c399c250c0b2539
Greg Warchol <gre8ory1@gmail.com>
2020-09-30 08:54:20 -0400

    Basic working demo

---
+++ lib/home_screen.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:open_calc/pixel_map.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+  final List<int> cursorLocation = [50,50];
+
+
+  Future<ui.Image> _makeImage(){
+    Color oldGreen = Color.fromRGBO(170, 200, 154, 1);
+    int width = 1080;
+    int height = 650;
+    final c = Completer<ui.Image>();
+    PixelMap pixelMap = PixelMap(1080, 650, oldGreen);
+    pixelMap.addLegend(cursorLocation);
+
+    decodeImageFromPixels(pixelMap.asList(), pixelMap.width, pixelMap.height, PixelFormat.rgba8888, c.complete);
+
+    return c.future;
+  }
+
+  void moveCursor(String direction){
+    setState(() {
+      if(direction == "UP"){
+        cursorLocation[1] += 10;
+      }else if(direction == "DOWN"){
+        cursorLocation[1] -= 10;
+      }else if(direction == "RIGHT"){
+        cursorLocation[0] += 10;
+      }else if(direction == "LEFT"){
+        cursorLocation[0] -= 10;
+      }
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+              FutureBuilder<ui.Image>(
+                future: _makeImage(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return RawImage(
+                      image: snapshot.data,
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [Column(
+                children: [
+                  InkWell(
+                      onTap: (){
+                        moveCursor('UP');
+                      },
+                    child: Icon(Icons.arrow_upward)
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(right: 25),
+                          child:InkWell(
+                              onTap: (){
+                                moveCursor('LEFT');
+                              },
+                            child: Icon(Icons.arrow_back)
+                          )
+                      ),
+                      InkWell(
+                          onTap: (){
+                            moveCursor('RIGHT');
+                          },
+                          child: Icon(Icons.arrow_forward)
+                      )
+                    ],
+                  ),
+                  InkWell(
+                      onTap: (){
+                        moveCursor('DOWN');
+                      },
+                      child: Icon(Icons.arrow_downward)
+                  ),
+                ],
+              )]
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
