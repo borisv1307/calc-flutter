97e4339fd8865b9df7d375f36322bbed70ce7572
Greg Warchol <gre8ory1@gmail.com>
2020-10-06 21:48:03 -0400

    Removed unnecessary logging

--- lib/bridge/graph_bridge.dart
+++ lib/bridge/graph_bridge.dart
@@ -24,7 +24,6 @@ class GraphBridge {
 
   CalcPoints _retrieveFunction(){
      if(_add == null){
-       print('new');
       final DynamicLibrary addNative = Platform.isAndroid
           ? DynamicLibrary.open("libcalc_2.so")
           : DynamicLibrary.process();
