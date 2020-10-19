6d2ba63e926156d245855f84e7ee837ec8126976
Blake Hieu Nguyen <57298440+nvthblake@users.noreply.github.com>
2020-10-18 20:11:24 -0400

    Add Scale Window

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -42,12 +42,27 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  // Add a Drawer for scale window
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  void _openDrawer() {
+    _scaffoldKey.currentState.openEndDrawer();
+  }
+
+  void _closeDrawer() {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     // than having to individually change instances of widgets.
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(widget.title),
+        actions: <Widget>[
+          // for hiding the drawer button
+          Container(),
+        ],
       ),
       body: ListView(
         shrinkWrap: true,
@@ -96,6 +111,133 @@ class _HomeScreenState extends State<HomeScreen> {
             )
           ])
         ],
+      ),
+      endDrawer: ClipRRect(
+        borderRadius: BorderRadius.vertical(
+            top: Radius.circular(15), bottom: Radius.circular(15)),
+        child: Container(
+          width: 200,
+          height: 470,
+          child: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('X min:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('X max:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('X scale:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Y min:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Y max:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('Y scale:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(child: Text('X res:')),
+                      Expanded(
+                        flex: 2,
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                ElevatedButton(
+                  onPressed: _closeDrawer,
+                  child: Text("Save Changes"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _openDrawer();
+        },
+        label: Text('Scale'),
+        icon: Icon(Icons.crop),
       ),
     );
   }
