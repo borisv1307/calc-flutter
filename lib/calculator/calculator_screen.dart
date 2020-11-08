45e6e51db8ad8028c50fb09189f0c3f647b45f44
Greg Warchol <gre8ory1@gmail.com>
2020-11-07 20:25:17 -0500

    Handle merge issues

--- lib/calculator/calculator_screen.dart
+++ lib/calculator/calculator_screen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:open_calc/bridge/graph_bridge.dart';
 import 'package:open_calc/calculator/calculator_display/calculator_display.dart';
+import 'package:open_calc/calculator/calculator_display/calculator_display_controller.dart';
 import 'package:open_calc/calculator/calculator_display/display_history.dart';
 import 'package:open_calc/calculator/input_pad/input_pad.dart';
 import 'package:open_calc/calculator/input_pad/input_variables.dart';
@@ -20,35 +21,28 @@ class CalculatorScreenState extends State<CalculatorScreen>{
   final VariableStorage storage;
   CalculatorScreenState(this.storage);
 
-  String displayString = '';    // user input as it should be displayed on screen (raw): '𝑥²'
   String userInputString = '';  // user input as it should be stored for calculating:  'x ^ 2'
   GraphBridge bridge = GraphBridge();
-  List<DisplayHistory> history = [];
   ValidateFunction tester = new ValidateFunction();
+  CalculatorDisplayController controller = CalculatorDisplayController();
 
   // updates state to display new input on the calc screen
   void displayInput(String keypadInput, String keypadInputDisplay) {
-    setState(() {
-      displayString = (displayString + keypadInputDisplay);
-      userInputString = (userInputString + keypadInput);
-    });
+      controller.input(keypadInput);
+      userInputString = controller.inputLine;
   }
 
   // updates state to perform special button commands
   void executeCommand(String command, String display) {
     if (command == 'enter') {
-      evaluate(userInputString, displayString);
+      evaluate(userInputString, controller.inputLine);
     } else if (command =='del') {
-      setState(() {
-        userInputString = userInputString.substring(0,userInputString.length-1); // TODO: fix to delete tokens instead of characters
-        displayString = displayString.substring(0,displayString.length-1);
-      });
+      userInputString = userInputString.substring(0,userInputString.length-1); // TODO: fix to delete tokens instead of characters
+      controller.delete();
     }else if(command =='clear'){
-      setState(() {
-        userInputString = '';
-        displayString = '';
-        history=[];
-      });
+      userInputString = '';
+      controller.inputLine = '';
+      controller.history=[];
     }else if(command =='sto'){
       var toSto = userInputString.split('(');
       var keyNum = toSto[0];
@@ -57,20 +51,16 @@ class CalculatorScreenState extends State<CalculatorScreen>{
 
       print(storage.variableMap);
 
-      setState((){
-        userInputString = '';
-        displayString = '';
-      });
+      userInputString = '';
+      controller.inputLine = '';
     }
   }
 
   // evaluates a function and adds the input to the history
   void evaluate(String expression, String displayExpression) {
-    print(userInputString);
-    print(displayString);
     String resultString;
     if (expression?.isEmpty ?? true) {  // empty string or null
-      resultString = (history.length == 0) ? "" : history.last.result;
+      resultString = (controller.history.length == 0) ? "" : controller.history.last.result;
     } else {
       bool validInput = tester.testFunction(expression);
       debugPrint("UI validation Check: $validInput");
@@ -82,12 +72,10 @@ class CalculatorScreenState extends State<CalculatorScreen>{
       }
     }
     DisplayHistory newEntry = new DisplayHistory(expression, displayExpression, resultString);
-    setState(() {
-      userInputString = '';
-      displayString = '';
-    });
+    userInputString = '';
+    controller.inputLine = '';
 
-    history.add(newEntry);
+    controller.history.add(newEntry);
   }
   
   @override
@@ -97,9 +85,8 @@ class CalculatorScreenState extends State<CalculatorScreen>{
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           CalculatorDisplay(
-            numLines: 8, 
-            inputLine: displayString, 
-            history: history
+            controller,
+            numLines: 8
           ),
           Expanded(
             child: InputPad(storage, displayInput, executeCommand)
