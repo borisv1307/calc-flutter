d54520a4d929718dd35a1362e32076d9223535c9
Ian Franklin <ianaf100@gmail.com>
2021-02-15 17:45:09 -0500

    additional table improvements, visual changes

--- lib/graph/graph_screen/graph_details/graph_table/graph_table.dart
+++ lib/graph/graph_screen/graph_details/graph_table/graph_table.dart
@@ -12,14 +12,12 @@ class GraphTable extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => GraphTableState();
-
 }
 
 class GraphTableState extends State<GraphTable> {
   List<List<Coordinates>> coordinates = [];
   int offset = 0;
   List<double> xValues;
-  bool showLeftArrow = false;
 
   @override
   void initState() {
@@ -81,71 +79,82 @@ class GraphTableState extends State<GraphTable> {
 
   @override
   Widget build(BuildContext context) {
-    // double devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
-    // double screenHeight = MediaQuery.of(context).size.height * devicePixelRatio;
-    // double appBarHeight = Scaffold.of(context).appBarMaxHeight * devicePixelRatio;
-    // double tableHeight = (screenHeight - (InteractiveGraphState.GRAPH_HEIGHT + appBarHeight)) / devicePixelRatio;
-
-    showLeftArrow = (offset > 0) ? true : false;
+    bool showLeftArrow = (offset > 0) ? true : false;
 
     return Container(
       child: Stack(
         children: <Widget>[
-            Center(
+          Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Center(child: Container(
-                  padding: EdgeInsets.fromLTRB(25,8,8,8),
-                  // color: Colors.blue[200],
-                  height: 60,
+                  padding: EdgeInsets.only(left: 3.0),
+                  height: 56,
+                  color: Colors.grey[300],
                   child: Row(
                     children: <Widget>[
                       Flexible(
+                        child: Container(
+                          width: 70 - 3.0,
+                          child: Center(child: Text("x", style: TextStyle(fontSize: 24))),
+                        ),
+                      ),
+                      Container(height: 56, child: VerticalDivider(color: Colors.black54, width: 2)),
+                      Flexible(
                         child: ListTile(
-                          title: Text("x", style: TextStyle(fontSize: 24))
+                          title: Center(child: Text("y" + subscript(offset + 1), style: TextStyle(fontSize: 24))),
+                          contentPadding: EdgeInsets.only(bottom: 9)
                         ),
                       ),
                       Flexible(
                         child: ListTile(
-                          title: Text("y" + subscript(offset + 1), style: TextStyle(fontSize: 24))
+                          title: Center(child: Text("y" + subscript(offset + 2), style: TextStyle(fontSize: 24))),
+                          contentPadding: EdgeInsets.only(bottom: 9)
                         ),
                       ),
                       Flexible(
                         child: ListTile(
-                          title: Text("y" + subscript(offset + 2), style: TextStyle(fontSize: 24))
+                          title: Center(child: Text("y" + subscript(offset + 3), style: TextStyle(fontSize: 24))),
+                          contentPadding: EdgeInsets.only(bottom: 9)
                         ),
                       ),
-                      Flexible(
-                        child: ListTile(
-                          title: Text("y" + subscript(offset + 3), style: TextStyle(fontSize: 24))
-                        ),
-                      ),
-                      
-                  ])
+                    ]
+                  )
                 )),
-                Divider(thickness: 1.5),
+                Divider(color: Colors.black38, thickness: 1, height: 1),
                 Expanded(
                   child: Container(
                     child: ListView.separated(
                       // controller: ScrollController(initialScrollOffset: 135*72.1),
                       shrinkWrap: true,
-                      padding: const EdgeInsets.all(8),
                       itemCount: xValues.length,
                       itemBuilder: (context, int index) {
-
                         List<Widget> row = [];
                         row.add(Flexible(
-                          child: ListTile(
-                            title: Text('${parseNumber(xValues[index])}', style: TextStyle(fontSize: 20))
+                          child: Container(
+                            width: 70,
+                            child: Center(child: Text('${parseNumber(xValues[index])}',
+                                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                maxLines: 1,
+                                overflow: TextOverflow.clip,
+                              ),
+                            ),
+                            color: Colors.grey[300],
+                            height: 56
                           ),
                         ));
 
+                        row.add(Container(height: 56, child: VerticalDivider(color: Colors.black, width: 2)));
+                        
                         for (int i = 0; i < coordinates.length && i < 3; i++) {
                           row.add(Flexible(
                             child: ListTile(
-                              title: Text('${parseNumber(coordinates[i][index].y)}', style: TextStyle(fontSize: 20))
+                              title: Center(child: Text('${parseNumber(coordinates[i][index].y)}', style: TextStyle(fontSize: 20),
+                                overflow: TextOverflow.clip,
+                                maxLines: 1,
+                              ))
                             ),
                           ));
                         }
@@ -154,12 +163,14 @@ class GraphTableState extends State<GraphTable> {
                           row.add(Spacer(flex: 2));
                         if (coordinates.length == 2) 
                           row.add(Spacer(flex: 1));
+                        if (coordinates.length == 0) 
+                          row.add(Spacer(flex: 3));
 
                         return Row(
                           children: row
                         ); 
                       },
-                      separatorBuilder: (BuildContext context, int index) => Divider(thickness: 1.5),
+                      separatorBuilder: (BuildContext context, int index) => Divider(thickness: 2, height: 2),
                     ),
                   ),
                 ),
@@ -167,8 +178,7 @@ class GraphTableState extends State<GraphTable> {
             ),
           ),
           Center(
-            child: 
-            Row(
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Opacity(
@@ -176,13 +186,13 @@ class GraphTableState extends State<GraphTable> {
                   child: Material(
                     color: Colors.transparent,
                     child: !showLeftArrow ? null : InkWell(
-                        onTap: () {
-                          setState(() {
-                            offset -= 1;
-                            _updateCoordinates();
-                          });
-                        },
-                        child: Icon(Icons.keyboard_arrow_left, size: 36,)
+                      onTap: () {
+                        setState(() {
+                          offset -= 1;
+                          _updateCoordinates();
+                        });
+                      },
+                      child: Icon(Icons.keyboard_arrow_left, size: 36)
                     ),
                   ),
                 ),
@@ -197,7 +207,7 @@ class GraphTableState extends State<GraphTable> {
                           _updateCoordinates();
                         });
                       },
-                      child: Icon(Icons.keyboard_arrow_right, size: 36,)
+                      child: Icon(Icons.keyboard_arrow_right, size: 36)
                     ),
                   ),
                 ),
