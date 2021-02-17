48f5118b18156d9d0a09d4b92448f605d4985198
Greg Warchol <gre8ory1@gmail.com>
2021-02-17 14:19:33 -0500

    Corrected tests

--- test/graph/graph_screen/graph_navigator/graph_navigator_test.dart
+++ test/graph/graph_screen/graph_navigator/graph_navigator_test.dart
@@ -7,9 +7,7 @@ import 'package:open_calc/graph/graph_screen/graph_navigator/graph_navigator.dar
 import 'package:open_calc/graph/graph_screen/graph_navigator/text_toggle_selection.dart';
 
 void main(){
-  Widget _buildTestableNavigator(Coordinates cursorLocation,Function(Coordinates updatedLocation) moveCursor){
-    GraphCursor cursor = GraphCursor();
-    cursor.location = cursorLocation;
+  Widget _buildTestableNavigator(GraphCursor cursor){
     ScaleSettings scaleSettings = ScaleSettings();
     scaleSettings.step = 3;
     return MaterialApp(home:GraphNavigator(cursor, TextToggleSelection('equations'), scaleSettings));
@@ -17,43 +15,35 @@ void main(){
 
   group('Arrows',(){
     testWidgets("can navigate cursor up", (WidgetTester tester) async {
-      Coordinates updatedCoordinates;
-      await tester.pumpWidget(_buildTestableNavigator(Coordinates(0,0),(Coordinates u){
-        updatedCoordinates = u;
-      }));
+      GraphCursor cursor = GraphCursor(Coordinates(0,0));
+      await tester.pumpWidget(_buildTestableNavigator(cursor));
       await tester.tap(find.byIcon(Icons.arrow_upward));
       await tester.pumpAndSettle();
-      expect(updatedCoordinates, Coordinates(0, 3));
+      expect(cursor.location, Coordinates(0, 3));
     });
 
     testWidgets("can navigate cursor right", (WidgetTester tester) async {
-      Coordinates updatedCoordinates;
-      await tester.pumpWidget(_buildTestableNavigator(Coordinates(0,0),(Coordinates u){
-        updatedCoordinates = u;
-      }));
+      GraphCursor cursor = GraphCursor(Coordinates(0,0));
+      await tester.pumpWidget(_buildTestableNavigator(cursor));
       await tester.tap(find.byIcon(Icons.arrow_forward));
       await tester.pumpAndSettle();
-      expect(updatedCoordinates, Coordinates(3, 0));
+      expect(cursor.location, Coordinates(3, 0));
     });
 
     testWidgets("can navigate cursor down", (WidgetTester tester) async {
-      Coordinates updatedCoordinates;
-      await tester.pumpWidget(_buildTestableNavigator(Coordinates(0,0),(Coordinates u){
-        updatedCoordinates = u;
-      }));
+      GraphCursor cursor = GraphCursor(Coordinates(0,0));
+      await tester.pumpWidget(_buildTestableNavigator(cursor));
       await tester.tap(find.byIcon(Icons.arrow_downward));
       await tester.pumpAndSettle();
-      expect(updatedCoordinates, Coordinates(0, -3));
+      expect(cursor.location, Coordinates(0, -3));
     });
 
     testWidgets("can navigate cursor left", (WidgetTester tester) async {
-      Coordinates updatedCoordinates;
-      await tester.pumpWidget(_buildTestableNavigator(Coordinates(0,0),(Coordinates u){
-        updatedCoordinates = u;
-      }));
+      GraphCursor cursor = GraphCursor(Coordinates(0,0));
+      await tester.pumpWidget(_buildTestableNavigator(cursor));
       await tester.tap(find.byIcon(Icons.arrow_back));
       await tester.pumpAndSettle();
-      expect(updatedCoordinates, Coordinates(-3, 0));
+      expect(cursor.location, Coordinates(-3, 0));
     });
   });
 }
